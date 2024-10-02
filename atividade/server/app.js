1ea610189021182d11b4ce97ef95b994d871c791
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-02 08:51:07 -0300

    banco de dados

--- atividade/server/app.js
+++ atividade/server/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import conectNaDB from './db.js';
 import cors from 'cors';
+import artista from './models/Artista.js';
 
 
 
@@ -10,15 +11,20 @@ app.use(cors());
 const conexao = await conectNaDB();
 
 conexao.on('error', (erro) =>{
-    console.error("Erro ao conectar no MongoDB", erro)
+   console.error("Erro ao conectar no MongoDB", erro)
 })
 
-conectNaDB.once('open', ()=>{
+conexao.once('open', ()=>{
     console.log('conectado no MongoDB')
 })
+
+app.get("/artistas",async(req,res) => {
+    const listaArtistas = await artista.find({});
+    res.status(200).json(listaArtistas)
+})
+
 
 app.listen(3000, ()=>{
     console.log("servidor conectado")
 })
 
-r
