86fead2ae33e8f49c512cbf521504e275332473e
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 09:50:16 -0300

    incompleto

--- atividade/src/App.jsx
+++ atividade/src/App.jsx
@@ -1,4 +1,4 @@
-//import { useState } from 'react'
+//import { useEffect, useState } from "react";
 import './index.css';
 import './output.css';
 import Home from './Pages/Home';
@@ -6,6 +6,16 @@ import Header from './Componentes/Header';
 /*npx tailwindcss -i ./src/index.css -o ./src/output.css --watch*/ 
 
 function App() {
+
+  // const [artistas,setArtistas] = useState([]);
+  // const [isLoading,setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/artistas')
+  //   .then (res => res.json())
+  //   .then (data => setArtistas(data))
+  //   .catch (err => console.log(err))
+  // },[])
 
   return (
     <>
--- atividade/src/Pages/Home.jsx
+++ atividade/src/Pages/Home.jsx
@@ -1,4 +1,5 @@
-import Conteudo from "../Componentes/conteudo";
+//import Conteudo from "../Componentes/conteudo";
+import { useEffect, useState } from "react";
 import Sidebar from "../Componentes/Sidebar";
 import Container from "../Componentes/Container";
 import MainConteudo from "../Componentes/MainConteudo";
@@ -6,45 +7,37 @@ import MainConteudo from "../Componentes/MainConteudo";
 
 function Home() {
 
+
+    const [artistas,setArtistas] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/artistas')
+    .then (res => res.json())
+    .then (data => setArtistas(data))
+    .catch (err => console.log(err))
+  },[])
+
+
     return ( 
         <>
         <Container>
             <Sidebar/>
             <MainConteudo>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            </MainConteudo>
-            {/* <div className=" w-full h-full flex flex-row flex-wrap p-16 justify-evenly">
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            <Conteudo/>
-            </div> */}
-        </Container>
-        {/* <div className='tudo h-lvh flex flex-col'>
-
-            <div className='MenuEConteudo flex flex-row w-full h-full'>
-
+                {
+                    artistas.map( artista => (
+                        <div className="conteudo bg-rosa size-40 flex flex-col items-center justify-evenly py-1 m-3">
+                            <p>{artista.name}</p>
+                            <div className="conteudoDoConteudo bg-ciano h-1/3 w-4/5"></div>
+                        </div>
+                    ))
+                }
+            <div className="conteudo bg-rosa size-40 flex flex-col items-center justify-evenly py-1 m-3">
+                <div className="conteudoDoConteudo bg-ciano h-1/3 w-4/5"></div>
+                <div className="conteudoDoConteudo bg-ciano h-1/3 w-4/5"></div>
             </div>
-        </div> */}
+            </MainConteudo>
+        </Container>
     </>
      );
 }
